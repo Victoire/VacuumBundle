0227b239fd7667573d3474c5490b3a2b024f031f
Made Wilson <a.grandin42@gmail.com>
2017-05-03 11:39:23 +0200

    Create BlogDataExtractorStagesTest

---
+++ Tests/Pipeline/WordPress/Stages/Blog/BlogDataExtractorStagesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Victoire\DevTools\VacuumBundle\Tests\Pipeline\WordPress\Stages;
+
+/**
+ * Class BlogDataExtractorStages
+ * @package Victoire\DevTools\VacuumBundle\Tests\Pipeline\WordPress\Stages
+ */
+class BlogDataExtractorStagesTest extends AbstractBaseStagesTests
+{
+    public function testPayloadIntegrity()
+    {
+
+    }
+}
