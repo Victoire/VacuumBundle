a99220412b540edfbc14fcd80ec348e254035c94
Made Wilson <a.grandin42@gmail.com>
2017-05-03 11:31:53 +0200

    Fix CategoryDataExtractorStagesTest

--- Tests/Pipeline/WordPress/Stages/Category/CategoryDataExtractorStagesTest.php
+++ Tests/Pipeline/WordPress/Stages/Category/CategoryDataExtractorStagesTest.php
@@ -3,6 +3,7 @@
 namespace Victoire\DevTools\VacuumBundle\Tests\Pipeline\WordPress\Stages\Category;
 
 use Victoire\DevTools\VacuumBundle\Entity\WordPress\Blog;
+use Victoire\DevTools\VacuumBundle\Entity\WordPress\Category;
 use Victoire\DevTools\VacuumBundle\Pipeline\WordPress\Stages\Category\CategoryDataExtractorStages;
 use Victoire\DevTools\VacuumBundle\Tests\Pipeline\WordPress\Stages\AbstractBaseStagesTests;
 
@@ -20,5 +21,21 @@ class CategoryDataExtractorStagesTest extends AbstractBaseStagesTests
         $payload = $this->getFreshPayload($params, $xml, new Blog());
 
         $payload = call_user_func($stage, $payload);
+
+        $tmpBlog = new Blog();
+        for ($ii = 1; $ii < 6; $ii++) {
+            $category = new Category();
+            $category->setCategoryName("Category Test ".$ii);
+            $category->setCategoryNicename("category-test-".$ii);
+            $category->setCategoryParent(0);
+            $category->setId($ii);
+            $category->setXmlTag("category");
+            $tmpBlog->addCategory($category);
+        }
+
+        foreach ($tmpBlog->getCategories() as $key => $category) {
+            $expectedTag = $payload->getTmpBlog()->getCategories()[$key];
+            $this->assertEquals($category, $expectedTag);
+        }
     }
 }
