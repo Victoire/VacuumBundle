2094cfa298fe9c37a4d2acd93fa89224ae74624d
Made Wilson <a.grandin42@gmail.com>
2017-05-03 14:27:23 +0200

    Create Utils Class to provide Doctrine Mock Entity Fix test on payload integrity

---
+++ Tests/Utils/DoctrineMockProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Victoire\DevTools\VacuumBundle\Tests\Utils;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DoctrineMockProvider
+ */
+class DoctrineMockProvider extends TestCase
+{
+    /**
+     * Will return an Moked EntityManager repository
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    public function getEMMock($repositoryReturnValue = null)
+    {
+        $emMock = $this->createMock(EntityManager::class,
+            array('getRepository', 'getClassMetadata', 'persist', 'flush'), array(), '', false);
+
+        $emMock->expects($this->any())
+            ->method('getRepository')
+            ->will($this->returnValue($this->getFakeRepository($repositoryReturnValue)));
+
+        $emMock->expects($this->any())
+            ->method('getClassMetadata')
+            ->will($this->returnValue((object)array('name' => 'aClass')));
+
+        $emMock->expects($this->any())
+            ->method('persist')
+            ->will($this->returnValue(null));
+
+        $emMock->expects($this->any())
+            ->method('flush')
+            ->will($this->returnValue(null));
+
+        return $emMock;
+    }
+
+    /**
+     * Return an Mocked EntityRepository
+     *
+     * @param $expectedValue
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    public function getFakeRepository($expectedValue)
+    {
+        $repository = $this->createMock(EntityRepository::class);
+
+        $repository
+            ->method('findOneBy')
+            ->will($this->returnValue($expectedValue));
+
+        return $repository;
+    }
+}
