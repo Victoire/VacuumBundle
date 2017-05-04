149a27434129cc05ab0ea73b5ef8cf968351b9f0
Made Wilson <a.grandin42@gmail.com>
2017-05-04 14:33:54 +0200

    implements genenrateMediaFormaterMock() in MediaFormaterMockProvider

--- Tests/Utils/MockProvider/MediaFormaterMockProvider.php
+++ Tests/Utils/MockProvider/MediaFormaterMockProvider.php
@@ -2,12 +2,37 @@
 
 namespace Victoire\DevTools\VacuumBundle\Tests\Utils\MockProvider;
 
+use Victoire\Bundle\MediaBundle\Entity\Folder;
+use Victoire\Bundle\MediaBundle\Entity\Media;
 use Victoire\DevTools\VacuumBundle\Utils\Media\MediaFormater;
 
+/**
+ * Class MediaFormaterMockProvider
+ */
 class MediaFormaterMockProvider extends DoctrineMockProvider
 {
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
     public function generateMediaFormaterMock()
     {
-        $this->createMock(MediaFormater::class);
+        $mediaFormaterMock = $this->createMock(MediaFormater::class);
+
+        $mediaFormaterMock
+            ->method("generateBlogFolder")
+            ->willReturn($this->createMock(Folder::class))
+        ;
+
+        $mediaFormaterMock
+            ->method("cleanUrl")
+            ->willReturn("http://lorempixel.com/300/200/abstract")
+        ;
+
+        $mediaFormaterMock
+            ->method("generateImageMedia")
+            ->willReturn($this->createMock(Media::class))
+        ;
+
+        return $mediaFormaterMock;
     }
 }
