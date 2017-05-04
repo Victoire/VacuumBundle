9f161502e5734c2a0c39ffb3588e6fdd6e5f9072
Made Wilson <a.grandin42@gmail.com>
2017-05-04 16:48:01 +0200

    Add Libxml2 in ci to avoid error with html wrapper when using saveHtml on article content

--- circle/dependencies.sh
+++ circle/dependencies.sh
@@ -7,4 +7,12 @@ if [ -n "${RUN_NIGHTLY_BUILD}" ]; then
   echo "xdebug enabled"
 fi
 
+# Add libxml2 installation to avoid html wrapper issue
+# when using saveHtml method on article content
+wget ftp://xmlsoft.org/libxml2/LATEST_LIBXML2
+tar -zxvf LATEST_LIBXML2
+cd libxml2-2.9.4
+./configure --prefix=/usr/local/libxml2
+make install
+
 php -d memory_limit=-1 /usr/local/bin/composer install --prefer-dist
